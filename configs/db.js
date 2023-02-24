43193778001533326fd52576c5e4eaa35485dd84
soumya995 <soumyarb292@gmail.com>
2023-02-25 00:41:41 +0530

    added

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connect = () => {
-  mongoose.set('strictQuery', false);
   return mongoose.connect(`mongodb+srv://Soumya:Soumya@cluster0.sgtoufm.mongodb.net/myfirstdb?retryWrites=true&w=majority`);
   
 };
